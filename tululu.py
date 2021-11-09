96b5857b38cb10de03c6e3852cbd11fb2d760e73
delphython <delphython@gmail.com>
2021-11-09 16:14:18 +0300

    Update tululu.py

--- tululu.py
+++ tululu.py
@@ -6,9 +6,9 @@ url = "http://tululu.org/b1/"
 response = requests.get(url)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, "lxml")
-title_tag = soup.find("head").find("title")
+title_tag = soup.find("h1")
 title_text = title_tag.text
-heading_author, _, _ = title_text.split(",")
-heading, author = heading_author.split("-")
+print(title_text)
+heading, author = title_text.split("::")
 print(f"Заголовок: {heading.strip()}")
 print(f"Автор: {author.strip()}")
