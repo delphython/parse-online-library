0fae6eb8ff2b1e54c441f6e751541b7085b0d56a
delphython <delphython@gmail.com>
2021-11-09 16:14:40 +0300

    Add name of book to file name

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 
 import requests
-from dotenv import load_dotenv
+
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def get_file_name(url):
-    parsed_url = urlparse(url)
-    path = unquote(parsed_url.path)
-    _, file_name = os.path.split(path)
+def parse_book(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
-    return file_name
+    soup = BeautifulSoup(response.text, "lxml")
+    title_tag = soup.find("h1")
+    title_text = title_tag.text
+    heading_author = title_text.split("::")
+
+    if len(heading_author) == 1:
+        heading_author.append("no author")
+
+    return heading_author
 
 
 def check_for_redirect(response, url):
@@ -17,37 +26,36 @@ def check_for_redirect(response, url):
         raise requests.HTTPError()
 
 
-def download_file(url, filename, file_dir, params=None):
-    response = requests.get(url, params)
+def download_txt(url, filename, folder="books/"):
+    response = requests.get(url)
     response.raise_for_status()
 
-    file_path = os.path.join(file_dir, filename)
+    file_path = os.path.join(folder, sanitize_filename(filename))
 
     try:
         check_for_redirect(response, url)
         with open(file_path, "wb") as file:
             file.write(response.content)
+
+        return file_path
     except requests.exceptions.HTTPError:
         pass
 
 
 def main():
-    load_dotenv()
-
-    # dvmn_api_key = os.environ[""]
-
-    # file_url = "https://tululu.org/txt.php?id=32168"
-    # file_name = "Peski_Marsa.txt"
-
     books_amount = 10
-    books_dir_name = "books"
-    books_dir = os.path.join(os.getcwd(), books_dir_name)
+    books_folder_name = "books/"
+    books_dir = os.path.join(os.getcwd(), books_folder_name)
     os.makedirs(books_dir, exist_ok=True)
 
     for book_id in range(1, books_amount + 1):
-        book_file_name = f"id{book_id}.txt"
         book_file_url = f"https://tululu.org/txt.php?id={book_id}"
-        download_file(book_file_url, book_file_name, books_dir)
+        book_url = f"https://tululu.org/b{book_id}"
+        heading, author = parse_book(book_url)
+        book_file_name = f"{book_id}. {heading.strip()}.txt"
+        file_path = download_txt(
+            book_file_url, book_file_name, books_folder_name
+        )
 
 
 if __name__ == "__main__":
