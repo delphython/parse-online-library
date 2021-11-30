ee862139c50be27edbcd9e49af9b0540927dae6b
delphython <delphython@gmail.com>
2021-11-30 16:14:37 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,11 +1,10 @@
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 
-def main():
-    fiction_books_attributes_file = "fiction_books.json"
-
+def on_reload(file):
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html", "xml"]),
@@ -13,7 +12,7 @@ def main():
 
     template = env.get_template("template.html")
 
-    with open(fiction_books_attributes_file, "r") as my_file:
+    with open(file, "r") as my_file:
         books_json = my_file.read()
 
     books = json.loads(books_json)
@@ -22,6 +21,18 @@ def main():
 
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
+
+
+def main():
+    fiction_books_attributes_file = "fiction_books.json"
+
+    server = Server()
+
+    server.watch(
+        fiction_books_attributes_file, on_reload(fiction_books_attributes_file)
+    )
+
+    server.serve(root=".")
 
 
 if __name__ == "__main__":
